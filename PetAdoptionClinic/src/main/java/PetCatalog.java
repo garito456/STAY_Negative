1d1447eef2b7338ac1714797dc4e9f1f7dc0680f
KillerRaptor247 <dantehart3654@gmail.com>
2022-03-27 20:27:17 -0500

    Implemented Sign-in Demo Form

---
+++ PetAdoptionClinic/src/main/java/PetCatalog.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class PetCatalog {
+	
+	ArrayList<Pet> catalog;
+
+	public ArrayList<Pet> getCatalog() {
+		return catalog;
+	}
+
+	public void setCatalog(ArrayList<Pet> catalog) {
+		this.catalog = catalog;
+	}
+	
+	
+
+}
