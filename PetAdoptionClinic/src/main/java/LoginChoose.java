add977fdb226a1898762da8ec5f99d1d7711e5ac
KillerRaptor247 <dantehart3654@gmail.com>
2022-03-28 21:19:58 -0500

    Optimized LoginChoose Class

--- PetAdoptionClinic/src/main/java/LoginChoose.java
+++ PetAdoptionClinic/src/main/java/LoginChoose.java
@@ -1,49 +1,64 @@
-package main.java;
+
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class LoginChoose {
+public class LoginChoose extends JFrame implements ActionListener {
+	
+    
+    JPanel panel = new JPanel(new BorderLayout());
+
+    JPanel messagePanel = new JPanel();
+    JLabel message;
+    JPanel buttonPanel = new JPanel();
+    JButton customerButton;
+    JButton employeeButton;
+	
 
     public void createAndShowGUI() {
-        JFrame frame = new JFrame("Choose Login");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel(new BorderLayout());
-
-        JPanel messagePanel = new JPanel();
-        JLabel message = new JLabel("Hello! Please Choose Your Role");
-        JPanel buttons = new JPanel();
-        JButton customerButton = new JButton("I'm a Customer");
-        JButton employeeButton = new JButton("I'm a Employee");
-
-        customerButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                LoginFormCustomer loginFormCustomer = new LoginFormCustomer();
-                loginFormCustomer.createAndShowGUI();
-            }
-        });
-
-        employeeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                LoginFormEmployee loginFormEmployee = new LoginFormEmployee();
-                loginFormEmployee.createAndShowGUI();
-            }
-        });
+    	setDefaultCloseOperation(EXIT_ON_CLOSE);
+    	
+    	message = new JLabel("Hello! Please Choose Your Role");
+        customerButton = new JButton("I'm a Customer");
+        employeeButton = new JButton("I'm a Employee");
+        
         messagePanel.add(message);
-        buttons.add(customerButton);
-        buttons.add(employeeButton);
+        buttonPanel.add(customerButton);
+        buttonPanel.add(employeeButton);
         panel.add(messagePanel, BorderLayout.NORTH);
-        panel.add(buttons, BorderLayout.CENTER);
-
-        frame.add(panel);
-        frame.pack();
-        frame.setVisible(true);
+        panel.add(buttonPanel, BorderLayout.CENTER);
+        add(panel);
+        // add action listener to buttons
+        customerButton.addActionListener(this);
+        employeeButton.addActionListener(this);
+        
+        pack();
+        setVisible(true);
     }
+
+
+	public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+	        if (e.getSource().equals(customerButton)) {
+	        	System.out.println("CUSTOMER ALERT");
+	            LoginFormCustomer loginFormCustomer = new LoginFormCustomer();
+	            loginFormCustomer.createAndShowGUI();
+	            this.dispose();
+	        	
+	        } else if (e.getSource().equals(employeeButton)) {
+	            System.out.println("EMPLOYEE ALERT");
+	            LoginFormEmployee loginFormEmployee = new LoginFormEmployee();
+	            loginFormEmployee.createAndShowGUI();
+	            this.dispose();
+	            
+	        } else {
+	            System.out.println(e.getSource());
+	        }
+		
+	}
+
+
 
 }
