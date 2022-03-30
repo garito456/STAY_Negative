3d90d53a672e1a2e7fc5e12e22a9d0c35d4dc8e5
Ruiqi Zhao <ricky_zhao1@baylor.edu>
2022-03-29 22:34:27 -0500

    Add the EditForm

--- PetAdoptionClinic/src/main/java/test.java
+++ PetAdoptionClinic/src/main/java/test.java
@@ -5,14 +5,7 @@ import javax.swing.*;
  */
 public class test {
     public static void main(String[] args) {
-
-        try {
-            // create instance of the LoginForm
-//            LoginForm form = new LoginForm();
-            LoginChoose loginChoose = new LoginChoose();
-            loginChoose.createAndShowGUI();
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
+        EditForm editForm = new EditForm();
+        editForm.createAndShowGui();
     }
 }
